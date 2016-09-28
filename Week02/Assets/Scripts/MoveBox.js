427bd93223c30cfd503e7436aaa01abad3e3db55
brianrichardmccarthy <brianmccarthy1@outlook.ie>
2016-09-28 09:27:37 +0100

    Changing mini-map to a static camera that shows the whole map

---
+++ Week02/Assets/Scripts/MoveBox.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+private var switchHeight: boolean;
+
+function Start () {
+	switchHeight = false;
+}
+
+function Update () {
+	if (switchHeight) {
+		this.transform.position.y -= .05;
+	} else {
+		this.transform.position.y += .05;
+	}
+
+	if (this.transform.position.y >= 3) {
+		switchHeight = true;
+	} else if (this.transform.position.y <= 1) {
+		switchHeight = false;
+	}
+
+	transform.Rotate(0, 1.5, 0);
+}
